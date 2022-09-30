7db9abd317da1e1bfcb2b2105f1363c528518038
baba-s <shotababa1206@gmail.com>
2022-09-30 11:05:37 +0900

    add

--- Editor/SerializedObjectExtensionMethods.cs
+++ Editor/SerializedObjectExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 
 namespace Kogane
 {
@@ -9,7 +10,17 @@ namespace Kogane
             self.serializedObject.DoDrawDefaultInspectorWithoutScript();
         }
 
+        public static void OnInspectorGUIWithoutScript( this Editor self, Action<SerializedProperty> onGUI )
+        {
+            self.serializedObject.DoDrawDefaultInspectorWithoutScript( onGUI );
+        }
+
         public static bool DoDrawDefaultInspectorWithoutScript( this SerializedObject self )
+        {
+            return self.DoDrawDefaultInspectorWithoutScript( null );
+        }
+
+        public static bool DoDrawDefaultInspectorWithoutScript( this SerializedObject self, Action<SerializedProperty> onGUI )
         {
             using var scope = new EditorGUI.ChangeCheckScope();
 
@@ -22,7 +33,14 @@ namespace Kogane
                 var propertyPath = iterator.propertyPath;
 
                 if ( propertyPath == "m_Script" ) continue;
-                EditorGUILayout.PropertyField( iterator, true );
+
+                if ( onGUI == null )
+                {
+                    EditorGUILayout.PropertyField( iterator, true );
+                    continue;
+                }
+
+                onGUI( iterator );
             }
 
             self.ApplyModifiedProperties();
